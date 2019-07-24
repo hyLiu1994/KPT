9d0cf6fa832ee2f64b49970d3d341eeb01ea89af
unknown <1627465617@qq.com>
2019-07-24 12:20:40 +0800

    commit

--- plt.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-KPT_Result=[0.3424973703227374, 0.3037812412903854, 0.3015666808164247, 0.2665089268415772, 0.3122982058745505, 0.31807628579928304, 0.4606175138508871]
-Index_Of_KPT_Result=[]
-for index,item in enumerate(KPT_Result):
-    Index_Of_KPT_Result.append(index)
-plt.plot(Index_Of_KPT_Result,KPT_Result)
-plt.show()
